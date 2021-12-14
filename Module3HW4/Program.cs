15ad67a69fdab7913fdc42a855ff7daa13ab1bbc
Oleh Potomakha <potomakha@gmail.com>
2021-12-14 22:56:20 +0200

    Taks2 ready

--- Module3HW4/Program.cs
+++ Module3HW4/Program.cs
@@ -7,9 +7,10 @@ namespace Module3HW4
         public static void Main(string[] args)
         {
             var task1 = new Task1();
+            var task2 = new Task2();
             Console.WriteLine(task1.SumOfSum());
 
-            Console.ReadKey();
+            task2.LINQTesting();
         }
     }
 }
